2a9adea5f101f1be29e132039ddf7c166cb8d9c8
Michal Jajcaj <michal.jajcaj@martes-specure.com>
2018-09-28 23:13:58 +0200

    root change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("tco_calculator_root"));
 
 //some comment and some other comment
